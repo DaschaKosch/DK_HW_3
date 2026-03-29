e6195edfdf3cfc47dffee3efba9db2d9ee6cb6a2
Darya Koshman <mozhetbut@gmail.com>
2026-03-30 02:53:03 +0300

    Added  CssXpathExamples

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
@@ -14,6 +15,10 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests extends TestBase {
 
+    @BeforeEach
+    void setUp() {
+        Configuration.browserSize = "1920x1080";
+    }
 
     @Test
     void successfulFillTest() {
@@ -43,4 +48,90 @@ public class TextBoxTests extends TestBase {
         $("[id=output]").shouldNotBe(visible, Duration.ofSeconds(5));
     }
 
+    @Test
+    void fillAllFieldsDetailedTest() {
+        open("https://demoqa.com/automation-practice-form");
+        $("[id=firstName]").setValue("Alex");
+        $("[id=lastName]").setValue("Black");
+        $("[id=userEmail]").setValue("alex@black.com");
+        $("[id=gender-radio-1]").click();
+        $("[id=userNumber]").setValue("1020304050");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__day--001").click();
+        $("#subjectsInput").setValue("Chemistry").pressEnter();
+        $("#hobbies-checkbox-3").click();
+        $("[id=uploadPicture]").uploadFromClasspath("img.png");
+        $("[id=currentAddress]").setValue("123  Street");
+        $("[id=state]").click();
+        $("[id=react-select-3-option-0]").click();
+        $("[id=city]").click();
+        $("[id=react-select-4-option-0]").click();
+        $("[id=submit]").click();
+        $(".modal-content").shouldBe(visible);
+
+        $(".modal-body").shouldHave(
+                text("Alex"),
+                text("Black"),
+                text("alex@black.com"),
+                text("Male"),
+                text("1020304050"),
+                text("01 January,2000"),
+                text("Chemistry"),
+                text("Music"),
+                text("img.png"),
+                text("123  Street"),
+                text("NCR"),
+                text("Delhi")
+        );
+
+
+    }
+
+    @Test
+    void fillRequiredFieldsDetailedTest() {
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Darja");
+        $("#lastName").setValue("Koshman");
+        $("#gender-radio-2").click();
+        $("#userNumber").setValue("9999999999");
+        $("#submit").scrollTo().click();
+        $(".modal-content").shouldBe(visible);
+
+        $(".modal-body").shouldHave(
+                text("Darja"),
+                text("Koshman"),
+                text("Female"),
+                text("9999999999")
+        );
+        $("#closeLargeModal").click();
+
+
+    }
+    @Test
+    void negativeTest_emptyForm() {
+        open("https://demoqa.com/automation-practice-form");
+        $("#submit").scrollTo().click();
+        $(".modal-content").shouldNotBe(visible);
+    }
+    @Test
+    void negativeTest_shortPhone() {
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Andy");
+        $("#lastName").setValue("Bin");
+        $("#gender-radio-1").click();
+        $("#userNumber").setValue("123");
+        $("#submit").scrollTo().click();
+        $(".modal-content").shouldNotBe(visible);
+    }
+    @Test
+    void negativeTest_onlyFirstName() {
+        open("https://demoqa.com/automation-practice-form");
+
+        $("#firstName").setValue("Andy");
+        $("#submit").scrollTo().click();
+        $(".modal-content").shouldNotBe(visible, Duration.ofSeconds(5));
+        $("#firstName").shouldBe(visible);
+    }
 }
