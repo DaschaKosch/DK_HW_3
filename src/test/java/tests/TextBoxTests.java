07935e10ead362d7b79eb17eef8cc066e20b8968
Darya Koshman <mozhetbut@gmail.com>
2026-04-04 23:15:53 +0300

    simple testData at start of allTests hw5

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
@@ -10,11 +11,25 @@ import org.openqa.selenium.By;
 import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class TextBoxTests extends TestBase {
-
+    String firstName = "Alex";
+    String lastName = "Black";
+    String email = "alex@black.com";
+    String gender = "Male";
+    String phone = "1020304050";
+    String birthMonth = "January";
+    String birthYear = "2000";
+    String birthDay = "1";
+    String subject = "Chemistry";
+    String hobby = "Music";
+    String picture = "img.png";
+    String currentAddress = "123  Street";
+    String state = "NCR";
+    String city = "Delhi";
 
     @Test
     void successfulFillTest() {
@@ -47,39 +62,38 @@ public class TextBoxTests extends TestBase {
     @Test
     void fillAllFieldsDetailedTest() {
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Alex");
-        $("[id=lastName]").setValue("Black");
-        $("[id=userEmail]").setValue("alex@black.com");
-        //$("#genderWrapper").$(By.xpath(".//label[text()='Male']")).click();
-        $("[value=Male]").click();
-        $("[id=userNumber]").setValue("1020304050");
+        $("[id=firstName]").setValue(firstName);
+        $("[id=lastName]").setValue(lastName);
+        $("[id=userEmail]").setValue(email);
+        $("[value=" + gender + "]").click();
+        $("[id=userNumber]").setValue(phone);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("January");
-        $(".react-datepicker__year-select").selectOption("2000");
-        $(".react-datepicker__day--001").click();
-        $("#subjectsInput").setValue("Chemistry").pressEnter();
+        $(".react-datepicker__month-select").selectOption(birthMonth);
+        $(".react-datepicker__year-select").selectOption(birthYear);
+        $(By.xpath("//div[contains(@class, 'react-datepicker__day') and text()='" + birthDay + "']")).click();
+        $("#subjectsInput").setValue(subject).pressEnter();
         $("#hobbiesWrapper").$(By.xpath(".//label[text()='Music']")).click();
-        $("[id=uploadPicture]").uploadFromClasspath("img.png");
-        $("[id=currentAddress]").setValue("123  Street");
+        $("[id=uploadPicture]").uploadFromClasspath(picture);
+        $("[id=currentAddress]").setValue(currentAddress);
         $("#state").click();
-        $("#state").$(By.xpath(".//div[text()='NCR']")).click();
+        $("#state").$(By.xpath(".//div[text()='" + state + "']")).click();
         $("#city").click();
-        $("#city").$(By.xpath(".//div[text()='Delhi']")).click();
+        $("#city").$(By.xpath(".//div[text()='" + city + "']")).click();
         $("[id=submit]").click();
         $(".modal-content").shouldBe(visible);
 
-        $(".modal-body").shouldHave(text("Alex"));
-        $(".modal-body").shouldHave(text("Black"));
-        $(".modal-body").shouldHave(text("alex@black.com"));
-        $(".modal-body").shouldHave(text("Male"));
-        $(".modal-body").shouldHave(text("1020304050"));
-        $(".modal-body").shouldHave(text("01 January,2000"));
-        $(".modal-body").shouldHave(text("Chemistry"));
-        $(".modal-body").shouldHave(text("Music"));
-        $(".modal-body").shouldHave(text("img.png"));
-        $(".modal-body").shouldHave(text("123  Street"));
-        $(".modal-body").shouldHave(text("NCR"));
-        $(".modal-body").shouldHave(text("Delhi"));
+        $(".modal-body").shouldHave(text(firstName));
+        $(".modal-body").shouldHave(text(lastName));
+        $(".modal-body").shouldHave(text(email));
+        $(".modal-body").shouldHave(text(gender));
+        $(".modal-body").shouldHave(text(phone));
+        $(".modal-body").shouldHave(text(birthDay + " " + birthMonth + "," + birthYear));
+        $(".modal-body").shouldHave(text(subject));
+        $(".modal-body").shouldHave(text(hobby));
+        $(".modal-body").shouldHave(text(picture));
+        $(".modal-body").shouldHave(text(currentAddress));
+        $(".modal-body").shouldHave(text(state));
+        $(".modal-body").shouldHave(text(city));
 
         $("#closeLargeModal").click();
 
@@ -90,17 +104,17 @@ public class TextBoxTests extends TestBase {
     @Test
     void fillRequiredFieldsDetailedTest() {
         open("/automation-practice-form");
-        $("#firstName").setValue("Darja");
-        $("#lastName").setValue("Koshman");
-        $("[value=Female]").click();
-        $("#userNumber").setValue("9999999999");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("[value=" + gender + "]").click();
+        $("#userNumber").setValue(phone);
         $("#submit").scrollTo().click();
         $(".modal-content").shouldBe(visible);
 
-        $(".modal-body").shouldHave(text("Darja"));
-        $(".modal-body").shouldHave(text("Koshman"));
-        $(".modal-body").shouldHave(text("Female"));
-        $(".modal-body").shouldHave(text("9999999999"));
+        $(".modal-body").shouldHave(text(firstName));
+        $(".modal-body").shouldHave(text(lastName));
+        $(".modal-body").shouldHave(text(gender));
+        $(".modal-body").shouldHave(text(phone));
 
         $("#closeLargeModal").click();
 
@@ -115,9 +129,9 @@ public class TextBoxTests extends TestBase {
     @Test
     void negativeTest_shortPhone() {
         open("/automation-practice-form");
-        $("#firstName").setValue("Andy");
-        $("#lastName").setValue("Bin");
-        $("[value=Male]").click();
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("[value=" + gender + "]").click();
         $("#userNumber").setValue("123");
         $("#submit").scrollTo().click();
         $(".modal-content").shouldNotBe(visible);
@@ -126,7 +140,7 @@ public class TextBoxTests extends TestBase {
     void negativeTest_onlyFirstName() {
         open("/automation-practice-form");
 
-        $("#firstName").setValue("Andy");
+        $("#firstName").setValue(firstName);
         $("#submit").scrollTo().click();
         $(".modal-content").shouldNotBe(visible, Duration.ofSeconds(5));
         $("#firstName").shouldBe(visible);
